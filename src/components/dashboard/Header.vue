59f1b369c7b18da8fa08738c7f343b811b272eac
briskr <40001+briskr@users.noreply.github.com>
2018-12-13 16:41:50 +0800

    FIX menu style

--- src/components/dashboard/Header.vue
+++ src/components/dashboard/Header.vue
@@ -7,8 +7,8 @@
     </div>
     <div class="l-toolbar__fill">
       <ul class="l-dsh__menu o-menu o-menu--h o-menu--dark">
-        <li><router-link to="/admin/users">Users</router-link></li>
-        <li><router-link to="/admin/roles">Roles</router-link></li>
+        <li><router-link to="/admin/users" class="item__text">Users</router-link></li>
+        <li><router-link to="/admin/roles" class="item__text">Roles</router-link></li>
       </ul>
     </div>
     <corner-menu class="l-toolbar__right">
